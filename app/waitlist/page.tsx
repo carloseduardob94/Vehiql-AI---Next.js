f5edb0daaf44d57d61f61f7cd0a3eb8e50f93489
Carlos Eduardo <educonts08@gmail.com>
2025-06-29 18:23:16 -0300

    feat: setup supabase done - created waitlist page

---
+++ app/waitlist/page.tsx
@@ -0,0 +1,17 @@
+const WaitListPage = () => {
+  return (
+    <div className="mt-20 fixed w-full">
+      <div className="aspect-video w-full">
+        <iframe
+          style={{ width: "100%" }}
+          height="81%"
+          src="https://vehiqlbr-waitlist.created.app"
+          title="Waiting List Landing Page"
+          frameBorder="0"
+        ></iframe>
+      </div>
+    </div>
+  )
+}
+
+export default WaitListPage;
