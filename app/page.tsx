411a85902b11145d96667abbfedcd25913b2cabf
Carlos Eduardo <educonts08@gmail.com>
2025-06-29 16:53:21 -0300

    feat: created header - Sign In page - Sign Up page

--- app/page.tsx
+++ app/page.tsx
@@ -2,10 +2,8 @@ import { Button } from "@/components/ui/button";
 
 export default function Home() {
   return (
-    <div>
-      <Button variant="destructive">
-        Subscribe
-      </Button>
-    </div>
+    <>
+
+    </>
   );
 }
